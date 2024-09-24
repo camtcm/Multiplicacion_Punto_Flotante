bc96756e0edfa6338e47efa671a2b7e259963533
ccm2326 <camilacm2326@gmail.com>
2024-09-24 17:15:24 -0500

    Separar componentes

--- Mult.cpp
+++ Mult.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <bitset>
+#include <cmath>
+#include <cstring>
 using namespace std;
 
 bitset<32> floatToBinary(float);
+void componentes(float, int&, int&, int&);
 
 int main() {
     float x, y;
@@ -17,7 +20,22 @@ int main() {
 
 bitset<32> floatToBinary(float value) {
     int bits = *(int*)&value;    
-    return bitset<32>(bits); // Facil manipulacion
+    return bitset<32>(bits); // Manipulacion 
 }
 
 
+void componentes(float valor, int& sign, int& exponent, int& significand) {
+    bitset<32> bits = floatToBinary(valor);
+    
+    sign = (bits[31]) ? 1 : 0; // primer bit
+
+    exponent = 0;
+    for (int i = 30; i >= 23; --i) {
+        exponent = (exponent << 1) | bits[i]; // desplazar y agregar
+    }
+    
+    significand = 0;
+    for (int i = 22; i >= 0; --i) {
+        significand = (significand << 1) | bits[i];
+    }
+}
