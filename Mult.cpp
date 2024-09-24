8ff16adfe1a723f5afef1d531f711a14d5497a0e
ccm2326 <camilacm2326@gmail.com>
2024-09-24 17:31:32 -0500

    Multiplicacion: suma exponentes, resta bias

--- Mult.cpp
+++ Mult.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 bitset<32> floatToBinary(float);
 void componentes(float, int&, int&, int&);
+float multiply(float,float);
 
 int main() {
     float x, y;
@@ -14,15 +15,13 @@ int main() {
     cout << "Ingrese otro numero flotante: ";
     cin >> y;
 
-    cout << "x: " << floatToBinary(x) << endl;
-    cout << "y: " << floatToBinary(y) << endl;
+    // float resultado = multiply(x,y);
 }
 
 bitset<32> floatToBinary(float value) {
     int bits = *(int*)&value;    
     return bitset<32>(bits); // Manipulacion 
 }
-
 
 void componentes(float valor, int& sign, int& exponent, int& significand) {
     bitset<32> bits = floatToBinary(valor);
@@ -38,4 +37,23 @@ void componentes(float valor, int& sign, int& exponent, int& significand) {
     for (int i = 22; i >= 0; --i) {
         significand = (significand << 1) | bits[i];
     }
+}
+
+float multiply(float x, float y) {
+    if (x == 0.0f || y == 0.0f) {
+        return 0.0f;
+    }
+
+    int signR, exponentR, significandR; //resultado
+    int signX, exponentX, significandX;
+    int signY, exponentY, significandY;
+
+    componentes(x, signX, exponentX, significandX);
+    componentes(y, signY, exponentY, significandY);
+
+    // SUMAR EXPONENTES
+    exponentR = (exponentX + exponentY); 
+
+    // RESTAR EL BIAS
+    exponentR -= 127;
 }
